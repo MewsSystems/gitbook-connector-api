5eaa38eea7bc14e48f9b2b89bdff6681988f6916
Jan Vlnas <jan.vlnas@mews.com>
2024-05-17 09:49:11 +0200

    Fix config loading

--- _generator/config.js
+++ _generator/config.js
@@ -1,44 +1,44 @@
-import { parseArgs } from "node:util";
-import fs from "node:fs";
-import YAML from "yaml";
+import { parseArgs } from 'node:util';
+import fs from 'node:fs';
+import YAML from 'yaml';
 
-const CONFIG_PATH = "./config.yaml";
+const CONFIG_PATH = './config.yaml';
 
 function loadArgs() {
   const parsedArgs = parseArgs({
     options: {
       tags: {
-        type: "string",
-        alias: "t",
+        type: 'string',
+        alias: 't',
         multiple: true,
         default: [],
       },
       source: {
-        type: "string",
-        alias: "s",
+        type: 'string',
+        alias: 's',
       },
       output: {
-        type: "string",
-        alias: "o",
+        type: 'string',
+        alias: 'o',
       },
     },
   });
-
+  const tags =
+    parsedArgs.values.tags.length > 0 ? parsedArgs.values.tags : undefined;
   return {
-    source: parsedArgs.source || undefined,
-    output: parsedArgs.output || undefined,
-    tags: parsedArgs.tags || undefined,
+    source: parsedArgs.values.source || undefined,
+    output: parsedArgs.values.output || undefined,
+    tags,
   };
 }
 
 function loadYaml(path) {
-  return YAML.parse(fs.readFileSync(path, "utf8"));
+  return YAML.parse(fs.readFileSync(path, 'utf8'));
 }
 
 export function loadConfig() {
   const args = loadArgs();
   const yamlConfig = loadYaml(CONFIG_PATH);
-
   const config = {
     oasPath: args.source || yamlConfig.source,
     outputPath: args.output || yamlConfig.output,
@@ -48,7 +48,11 @@ export function loadConfig() {
     console.error(
       'Usage: node index.js --source <url-or-path-to-openapi> --output <output-folder> [--tags tag1 [--tags "some tag2"]]'
     );
-    throw new Error("Missing required arguments");
+    throw new Error('Missing required arguments');
+  }
+  // TODO: This check can be removed after initial migration; if no tags are provided, generator will pick up all tags by default
+  if (config.tags.length === 0) {
+    throw new Error('No tags specified');
   }
   return config;
 }
