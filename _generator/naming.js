9352a1908290edf54dd8c29a7aa282b7151b315b
Jan Vlnas <jan.vlnas@mews.com>
2024-05-20 15:30:07 +0200

    Handle empty description

--- _generator/naming.js
+++ _generator/naming.js
@@ -10,6 +10,6 @@ export function slugify(str) {
   return str.toLowerCase().replace(/[^a-z0-9]/g, '-');
 }
 
-export function firstLine(str) {
+export function firstLine(str = '') {
   return str.split(/\r\n|\r|\n/)[0];
 }
