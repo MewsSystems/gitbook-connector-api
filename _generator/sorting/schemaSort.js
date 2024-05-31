af5ee9010e63959555b94b31ca5516b757258ae5
Jonas Pauliukevičius <jonas.pauliukevicius@mews.com>
2024-05-31 11:35:20 +0200

    remove unnecessary schema order

--- _generator/sorting/schemaSort.js
+++ _generator/sorting/schemaSort.js
@@ -1,20 +1,4 @@
 const orderMap = {
-  service: 10,
-  serviceoptions: 10,
-  coproduct_servicedata: 11,
-  servicedatadiscriminator: 12,
-  servicetype: 13,
-  bookableservicedata: 14,
-  timeunit: 15,
-  'figure1:illustrationofanightlyservice': 16,
-  'figure2:illustrationofadaytimeservice': 17,
-  timeunitperiod: 18,
-  additionalservicedata: 19,
-  promotions: 20,
-  serviceavailabilitymetrics: 21,
-  'resourcecategoryavailability(ver2024-01-22)': 22,
-  resourcecategoryavailability: 23,
-  availabilityupdate: 24,
   default: 0,
 };
 
